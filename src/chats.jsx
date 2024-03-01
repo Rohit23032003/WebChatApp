cd9c99dabb6585f0166c8e7dfc3989a8eeaed9a2
Rohit23032003 <sohitrohit1717@gmail.com>
2024-03-01 16:32:49 +0530

    receiverend logic

--- src/chats.jsx
+++ src/chats.jsx
@@ -1,150 +1,3 @@
-// import { useEffect, useState, useMemo } from "react"
-// import axios from 'axios';
-// import { useParams } from "react-router-dom";
-// import { io } from 'socket.io-client';
-
-
-// const ChatPage = () =>{
-
-//     const [socket, setSocket] = useState(io('http://localhost:8000', {
-//         reconnection: false // Disable automatic reconnection
-//     }));
-//     const [users , setUsers] =useState([{}]);
-//     const [receiverId , setReceiverId] = useState();
-//     const [chats , setChats] = useState([]);
-//     const { Id } = useParams();
-//     const senderId = Id.slice(1);
-//     const [message , setMessage] = useState();
-
-
-//     useEffect(() => {
-//         // Setup socket connection and event listeners
-//         socket.on('connect', () => {
-//             console.log("connected", socket.id);
-//             socket.emit('setUserId', senderId);
-//         });
-    
-//         socket.on('ReceiveMessage', (newChat) => {
-//             setChats((chats) => ([...chats, newChat]));
-//         });
-    
-//         // Clean up function to remove event listeners
-//         return () => {
-//             socket.disconnect();
-//             socket.off('ReceiveMessage');
-//         };
-//     }, [senderId]);
-    
-
-//     useEffect(()=>{
-//         const fethingUsers = async() => {
-//             try {
-//                 const fetchedUsers = await axios.get('http://localhost:8000/user',
-//                 {withCredentials:true});
-//                 setUsers(fetchedUsers.data.users);
-//             } catch (error) {
-//                 console.log(error);
-//             }
-//         }
-//         fethingUsers();
-
-//     },[]);
-
-
-//     useEffect(()=>{
-//         const fetchChats = async () =>{
-//             try {
-//                 console.log('sender id in params ' , senderId , "receiver id " , receiverId);
-//                 const user = {
-//                     senderId , receiverId
-//                 }
-//                 const fetchedChats = await axios.post( 'http://localhost:8000/user/personalchats',user ,
-//                     {withCredentials:true});
-//                 console.log(fetchedChats.data);
-//                 if(fetchedChats.data.success){
-//                     console.log("data is " , fetchedChats.data.chats);
-//                     setChats([...fetchedChats.data.chats]);
-//                 }
-
-//             } catch (error) {
-//                 console.log(error);
-//             }
-//         }
-//         fetchChats();
-        
-//     },[receiverId]);
-
-//     const sendMessage = async()=>{
-
-//         setMessage((message) => message.trim());
-
-//             if(message.length > 0) {
-//                 const storeMessageDb = async() => {
-//                     try {
-//                         const chat = {
-//                             senderId , receiverId , message
-//                         }
-//                         const response = await axios.post("http://localhost:8000/user/chats", chat , {
-//                             withCredentials:true
-//                         } );
-
-//                             console.log(response.data);
-//                         if(response.data.success){
-//                             console.log(response.data.newChat);
-//                             const msg = response.data.newChat
-//                             setChats((chats) => ([...chats , msg]));
-//                             socket.emit('sendingMessage' , {receiverId , msg});
-//                         }
-//                     } catch (error) {
-//                         console.log("error while sending chat " , error);
-//                     }
-//                 }
-//                 storeMessageDb();
-//                 setMessage("");
-//             }
-//     }
-
-//     return (
-//         <>
-//             {
-//                 users.map((user)=>{
-//                     return (   
-//                         <div onClick={()=> setReceiverId(user._id)}>
-//                             {
-//                                user._id + " " + user.userName
-//                             }
-//                         </div>
-//                     )
-//                 })
-//             }
-//             <br/>
-//             <br/>
-//             {
-//                 chats.map((chat)=>{
-//                     return (
-//                         <div>
-//                             {`${chat.message} from ${senderId} to ${receiverId}`}
-//                         </div>
-//                     )
-//                 })
-//             }
-//             <br/>
-//             <br/>
-//             <div>
-//                 <input type="text" value={message}
-//                 placeholder="type message"  
-//                     onChange={(e)=> setMessage(e.target.value)}
-//                 />
-//                 <button onClick={sendMessage}>send</button>
-//             </div>
-//         </>
-//     )
-// }
-
-// export default ChatPage;
-
-
-
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import { useParams } from "react-router-dom";
@@ -158,6 +11,7 @@ const ChatPage = () => {
     const { Id } = useParams();
     const senderId = Id.slice(1);
     const [message, setMessage] = useState("");
+
 
     useEffect(() => {
         // Initialize socket connection
@@ -175,7 +29,10 @@ const ChatPage = () => {
         });
 
         newSocket.on('ReceiveMessage', (newChat) => {
-            setChats(chats => [...chats, newChat]);
+            console.log(newChat);
+            if(senderId === newChat.receiverId){
+                setChats(chats => [...chats, newChat]);
+            }
         });
 
         // Handle connection errors
